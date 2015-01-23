60b8f48f776587cdc1331dab3c68780066191009
Benoit Parmentier <benoit.parmentier@gmail.com>
2015-01-23 10:45:00 -0500

    fitting variogram with cutoff range and specific lag classes + degugging

--- interpTest1.R
+++ interpTest1.R
@@ -1,6 +1,14 @@
-# starting a session
+####################################  INTERPOLATION OF Traffic   #######################################
+############################  Script for kriging of traffic    ##############################
+#This script tests various variogram models for kriging of traffic volume data.
+#AUTHORS: Sean McFall, Benoit Parmentier 
+#CREATED ON: 01/12/2015 ? (change here) 
+#MODIFIED ON: 01/23/2015            
+#Version: 1
+#PROJECT: Sean McFall's WM?? (change here)                                     
+#################################################################################################
 
-###Loading R library and packages                                                      
+### Loading R library and packages        
 
 library(geoR)
 library(rgdal)
@@ -111,7 +119,6 @@ tb_hscat <- hscat(AADT~1,OR_spdf,breaks_hscat,as.table=T)
 ## variogram plot, cloud
 variogram(AADT ~ 1, ID)
 vgm1 <- variogram(AADT~1,OR_spdf,width=(3:18) * 100) #need better control range and bin/lag distance here?
-vgm1 <- variogram(AADT~1,OR_spdf,cutoff) #need better control range and bin/lag distance here? I think we can use cutoff
 
 ##other possiblities?
 #-other variogram package translated in gstat
@@ -154,7 +161,25 @@ kriging_result = autoKrige(AADT ~ 1, input_data = data, new_data = s_spdf,data_v
 # variograms
 #
 
+######################### NOW taking what has beeen learned above ##############
 # variogram comparisons
+
+OR_rast <- raster(ex_OR)
+res(OR_rast) <- c(100,100)
+proj4string(OR_rast) <- aea
+#setValues(OR_rast) <- rep(1, ncell(OR_rast))
+values(OR_rast) <- rep(1, ncell(OR_rast))
+
+# covert to griddf
+OR_grid <- as(OR_rast, 'SpatialGridDataFrame')
+s_sgdf <- as(OR_rast, 'SpatialGridDataFrame')
+
+#s_sgdf<-as(r_stack_covar,"SpatialGridDataFrame") #Conversion to spatial grid data frame, only convert the necessary layers!!
+s_spdf<-as.data.frame(s_sgdf) #Note that this automatically removes all NA rows
+s_spdf<-na.omit(s_spdf) #removes all rows that have na...
+coords<- s_spdf[,c('s1','s2')]
+coordinates(s_spdf)<-coords
+proj4string(s_spdf)<-proj4string(s_sgdf) #Need to assign coordinates...
 
 #Read in ArcGIS variogram and compare to R gstat
 #
@@ -163,6 +188,53 @@ kriging_result = autoKrige(AADT ~ 1, input_data = data, new_data = s_spdf,data_v
 #
 #
 
+#Use cutoff to cut variogram...
+vgm1 <- variogram(AADT~1,OR_spdf,cutoff=10000) #need better control range and bin/lag distance here? I think we can use cutoff
+plot(vgm1)
+vgm1 <- variogram(AADT~1,OR_spdf,width=200,cutoff=10000) #need better control range and bin/lag distance here? I think we can use cutoff
+plot(vgm1)
+#20km is probably too long, set to 10km for now? could be also defined on distrubtion of distances from input...
+vgm1 <- variogram(AADT~1,OR_spdf,width=200,cutoff=20000) #need better control range and bin/lag distance here? I think we can use cutoff
+plot(vgm1)
 
+vgm1 <- variogram(AADT~1,OR_spdf,width=200,cutoff=10000) #need better control range and bin/lag distance here? I think we can use cutoff
+plot(vgm1) #this is the sample variogram
 
+vgm_model <- vgm(psill=1,model="Exp",range=10000/3,nugget=1) #define theoretical model for the variogram...
+#read the documentation to set the values above in some sensible way...
 
+vg_fit <- fit.variogram(vgm1, model=vgm_model) #now fit
+#vg_fit <- fit.variagram(vgm1, model=vgm(1,"Sph",1800,1),fit.ranges=FALSE)
+plot(vg_fit)
+
+#Use krige or krige0 function from gstat package to set the variogram used in the modleing
+#x - krige(log(zinc)~1, meuse, meuse.grid, model = m, beta = 5.9)
+
+data <- remove.duplicates(OR_spdf)#, zero = dist_val) #spatially sub sample...
+krige_mod <- try(krige(AADT~1, OR_spdf, newdata= s_spdf, model = vg_fit))#, beta = 5.9)
+#we need to have a vg_fit that has a limited range to predict ....
+
+#TODO:
+#chekc if cutoff option of fix.value can be used to set the range or cutoff variagrma in the Automap package...
+#chekc the error for krige:
+#"chfactor.c", line 131: singular matrix in function LDLfactor() Error in predict.gstat(g, newdata = newdata, block = block, nsim = nsim,  :    LDLfactor
+#from the google search...:
+#Yes. Also near-identical locations in combination with variograms 
+#without a nugget effect may caus it.
+
+#The GSTAT online manual suggests to remove "noaverage" or increase "zero"
+#to overcome the problem. However, I have not figured out how to do this
+#when running gstat under R.
+
+#Either package gstat, but more likely package sp has a function zerodist 
+#which will find point pairs with (nearly) zero distances. You may use 
+#the indexes it returns to do something with these points (e.g. remove 
+#one of them).
+
+#Should I use only a subset of the orignial data or are there other
+#alternatives to overcome the problem?
+
+#Yes. An alternative would be to average observations, if they are scalar 
+#data.
+
+#Maybe once the grid is created average one observation by grid cell and use this new sample point dataset in the kriging? 
