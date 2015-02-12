3e7d498900b553f4a6d98b1d862ce200a259cd28
Sean <sean.l.mcfall@gmail.com>
2015-02-12 13:20:10 -0500

    wrote a function that automatically pulls out proj4string from shapefile

--- functions.R
+++ functions.R
@@ -9,6 +9,33 @@
 #
 #
 
+
+
+getProj <- function(vct_name) {
+  
+  #############
+  
+  # INPUTS
+  # vct_name  = vector name, including file ending
+  
+  # OUTPUTS
+  # proj4string = proj4string
+  
+  #############
+  
+  # run system command
+  # outputs to temp txt file
+  system(paste("gdalsrsinfo", vct_name, "> proj4str.txt", sep = " "))
+  
+  # get second line of txt file
+  proj4string <- readLines("proj4str.txt", n = 2)
+  
+  # save second line as variable
+  proj4string <- substr(proj4string[2], 10, 200)
+  
+  return(proj4string)
+}
+
 createSpdf <- function(vct_name, vct_dir, proj, col_names) {
   
   #############
@@ -17,7 +44,7 @@ createSpdf <- function(vct_name, vct_dir, proj, col_names) {
   # vct_name  = vector name, including file ending
   # vct_dir   = vector directory
   # proj      = proj4string
-  # col_names = column name
+  # col_names = column names, form of a vector! (an ice dragon!)
   
   # OUTPUTS
   # spdf      = spatial points data frame that contains desired column/s
@@ -26,6 +53,12 @@ createSpdf <- function(vct_name, vct_dir, proj, col_names) {
   
   # bring vector file into memory
   vector <- readOGR(dsn = vct_dir, layer = vct_name) #, p4s = NAD27)
+  
+  # get proj4string
+  
+  # run system command to pull .prj info
+  
+  # access as string
 
   # convert targeted column into df
   vector_df <- as.data.frame(vector$col_names)
@@ -36,6 +69,5 @@ createSpdf <- function(vct_name, vct_dir, proj, col_names) {
   # set spdf projection, column names
   proj4string(vector_spdf) <- proj
   names(vector_spdf) <- col_names
-  
   
 }
