d9320a40771cbe127af81afd2778ed9261b1b746
Sean <sean.l.mcfall@gmail.com>
2015-04-07 11:54:12 -0400

    moved the main function into the functions file

--- main.R
+++ main.R
@@ -14,7 +14,7 @@ aea <- "+proj=aea +lat_1=38 +lat_2=41 +lat_0=34 +lon_0=-114 +x_0=0 +y_0=0 +datum
 cUS <- readOGR(dsn = "/home/sean/Documents/trafficVolume/cUS.shp", layer = "cUS")
 
 # state to be buffered
-stateAbbreviations <- list("OR")
+stateAbbreviations <- list("OR", "NV", "CA")
 
 analysis <- lineprof(main(state.list = stateAbbreviations, column.names = c("AADT"), equation = c("AADT ~ 1"), 
                           data.suffix = data.suffix, data.directory = data.directory, resolution = 1000, 
@@ -22,65 +22,6 @@ analysis <- lineprof(main(state.list = stateAbbreviations, column.names = c("AAD
 
 #####
 
-main <- function(state.list, column.names, equation, data.suffix, data.directory, resolution, projection) {
-  
-  for (state.name in state.list){
-
-    adjacent.states <- getAdjStates(state.abbrv = state.name)
-    
-    state.ranges <- getStateRanges(adj.states = adjacent.states, proj.str = projection, 
-                                   col.names  = column.names, equation = equation, 
-                                   pnt.suffix = data.suffix, pnt.dir = data.directory, 
-                                   rst.res    = resolution
-                                   )
-    
-    buffered.spdf <- getBufferedSpdf(state.abbrv = state.name, pnt.dir = data.directory,
-                                     col.names   = column.names, proj.str = projection,
-                                     ranges.df   = state.ranges, pnt.suffix = data.suffix,
-                                     adj.states  = adjacent.states, data.directory = data.directory
-                                     )
-    
-    log.spdf <- logTransformSpdf(buffered.spdf, column.names, projection)
-    validation.spdf.list <- createValidationData(log.spdf, .3)
-    training.spdf <- validation.spdf.list[[1]]
-    test.spdf <- validation.spdf.list[[2]]
-    
-    tr.raster <- createRaster(spdf = training.spdf, resolution = resolution )
-    tr.avg.spdf <- createAvgSpdf(spdf = training.spdf, rast = tr.raster, col_names = column.names)
-    tr.cutoff <- getBufferCutoff(spdf = training.spdf)
-    tr.width <- getBufferWidth(rast = tr.raster)
-    tr.variogram <- createVariogram(equation = equation, spdf = training.spdf, 
-                                    varWidth = tr.width, varCutoff = tr.cutoff)
-    # save variogram
-    
-    tr.grid <- as(tr.raster, 'SpatialGridDataFrame')
-    
-    tr.krige <- createKrigeLayer(spdf = training.spdf, grid = WA.grid, 
-                                  raster = tr.raster, equation = equation, 
-                                  variogram = tr.variogram, rst_name = state.name)
-    
-    # write out rasters, save them as objects too
-    writeKrigeLayer(krige.layer = tr.krige, extent.raster = tr.raster, name.prefix = state.name)
-    
-    tr.surfaces <- getKrigeRasters(krige.layer = tr.krige, extent.raster = tr.raster)
-    
-    tr.pred.rst <- tr.surfaces[[1]]
-    
-    tst.validation.dfs <- createValidationDataFrames(spdf = test.spdf, projection = projection,
-                                                     krige.surface = tr.pred.rst)
-    tr.validation.dfs <- createValidationDataFrames(spdf = training.spdf, projection = projection,
-                                                    krige.surface = tr.pred.rst)
-    
-    tr.err.vals <- getErrorValues(validation.spdfs = tr.validation.dfs, column.names = column.names)
-    tst.err.vals <- getErrorValues(validation.spdfs = tst.validation.dfs, column.names = column.names)
-    
-    output.list <- list(tr.krige, tr.validation.dfs, tst.validation.dfs, tr.variogram, tr.err.vals, tst.err.vals)
-    names(output.list) <- c("Krige Object", "Training Validation Table", "Testing Validation Table",
-                            "Variogram", "Training Error Values", "Testing Error Values")
-    
-    saveRDS(output.list, paste(state.name, "_output.rds", sep=""))
-  }
-}
 
 #
 # additional possible analysis of the data
